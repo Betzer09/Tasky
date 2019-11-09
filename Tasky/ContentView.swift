3e5567e60c1f2a3f427d7e119e0ccd236af6e7e6
Betzer09 <betzer.austin09@gmail.com>
2019-11-09 15:45:13 -0700

    inital project

---
+++ Tasky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tasky
+//
+//  Created by Austin Betzer on 11/9/19.
+//  Copyright © 2019 SPARQDEV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
