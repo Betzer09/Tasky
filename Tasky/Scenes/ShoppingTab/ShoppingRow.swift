5b5850363adf0d5ca8945ca0bd14a7d8bb6f5143
Betzer09 <betzer.austin09@gmail.com>
2019-11-10 12:20:53 -0700

    Populate cell based on complete and nonComplete

--- Tasky/Scenes/ShoppingTab/ShoppingRow.swift
+++ Tasky/Scenes/ShoppingTab/ShoppingRow.swift
@@ -19,12 +19,23 @@ struct ShoppingRow: View {
             Text("\(item.name)")
                 .font(.headline)
                 .fontWeight(.regular)
-        }.padding(4)
+            
+            Spacer()
+            
+            if item.isComplete {
+                Image("Checked")
+            } else {
+                Image("Unchecked")
+            }
+        }
+        .frame(maxWidth: .some(325))
+        .padding([.leading, .trailing], 12)
+        .padding([.top, .bottom], 10)
     }
 }
 
 struct ShoppingRow_Previews: PreviewProvider {
     static var previews: some View {
-        ShoppingRow(item: SHOPPING_ITEMS1.first!)
+        ShoppingRow(item: SHOPPING_ITEMS1[1])
     }
 }
